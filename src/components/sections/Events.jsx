3dee95b0bb0c3f34f494ceed23de495ba42ae875
Udula Ravishan <udula.ravishan.email@gmail.com>
2025-08-14 14:22:41 -0400

    Calender
    Calendar done

--- src/components/sections/Events.jsx
+++ src/components/sections/Events.jsx
@@ -1,8 +1,31 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 export const Events = () =>{
-    return(<section id="events" className="min-h-screen flex items-center justify-center relative">
-        <div className="text-center">
-            <h1 className="text-4xl">events coming soon....</h1>
+    return(<section id="events" className="py-12">
+        
+        <div className="max-w-5xl mx-auto px-4">
+            <h1 className="text-center font-bold text-5xl">Events</h1>
+            
+            <div className="rounded-xl text-white">
+                <FullCalendar 
+
+                    plugins={[dayGridPlugin]}
+                    initialView="dayGridMonth"
+                    height="auto"
+                    events={[
+                        {title:'Data Exploration & EDA Workshop', date: '2025-09-10'},
+                        {title:'Python Data Automation Challange', date: '2025-09-24'},
+                        {title:'Machine Learning Workshop', date: '2025-10-29'},
+                        {title:'Production Workshop', date: '2025-11-12'},
+                        {title:'Power BI Report Competition', date: '2025-11-26'}
+                    ]}
+            
+            />
+
+            </div>
+            
+            
             
 
         </div>
